3ee57b8e0537e2d5159071279e53daf68d2228d5
Baic <303394539@qq.com>
2022-08-02 17:41:19 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@umijs/max';
+
+import CompressionVitePlugin from 'vite-plugin-compression';
+
+export default defineConfig({
+  vite: {
+    plugins: [CompressionVitePlugin()],
+  },
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+        name: ' CRUD 示例',
+        path: '/table',
+        component: './Table',
+    },
+  ],
+  npmClient: 'pnpm',
+});
+
